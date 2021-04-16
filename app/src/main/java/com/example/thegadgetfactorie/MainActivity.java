f7fa109f76c497883e9c6a93ec13a64642c853c9
diyarenny <diya.renny@mydit.ie>
2021-04-16 13:02:51 +0100

    Adding forgot password feature. Making some UI changes.

--- app/src/main/java/com/example/thegadgetfactorie/MainActivity.java
+++ app/src/main/java/com/example/thegadgetfactorie/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.thegadgetfactorie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    //variables
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
     }
+
+
+    //main menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+
+    //main menu options
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_logout_btn:
+                logOut();
+                return true;
+
+            case R.id.action_settings_btn:
+                ///startActivity(new Intent(getApplicationContext(), SetupActivity.class));
+                return true;
+
+            default:
+                return false;
+
+        }
+    }
+
+    //on start if the user is not logged in then user is bought to the login page
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null){
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
+    }
+
+    //logs out the user and send back to the login page
+    private void logOut() {
+        firebaseAuth.signOut();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
 }
